b95fa7a051b5e150a284b2b8a84b728676466a29
mobbcitestjob4 <citestjob4@mobb.ai>
2025-12-22 22:08:48 +0000

    Test commit

--- xss-example.js
+++ xss-example.js
@@ -1,7 +1,3 @@
-// This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
-
-var urlParams = new URLSearchParams(window.location.search);
-var username = urlParams.get('username');
-
-var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.innerHTML = "Hello to you ";
+<html><body><h1>503 Service Unavailable</h1>
+No server is available to handle this request.
+</body></html>
